a37c7580eb3c94c45f8893592f0a66fdaf5f1ac3
sakaiken <sakaiken@sakaikennoMacBook-Pro.local>
2021-11-05 10:10:55 +0900

    Q4-Q7回答提出します

--- task.rb
+++ task.rb
@@ -25,14 +25,14 @@ def q3
   # 以下に回答を記載
   p numbers.count(3)
 
-
+3
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -42,21 +42,23 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts array1.empty?
+  puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map {|num| num * 10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
